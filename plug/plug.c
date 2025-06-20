482a44c7513508305d8302d6470a47b1a493bfbd
kushvlshresthv <kushvlshresthv@gmail.com>
2025-06-20 20:00:19 +0545

    implemented function to draw a cell

--- plug/plug.c
+++ plug/plug.c
@@ -1,20 +1,31 @@
 #include "plug.h"
-#include <stdio.h>
 
+
+//forward declarations:
+void draw_cell(SDL_Surface* surface, int cell_x, int cell_y);
 
 void update(Plug *plug) {
-
-    float cell_length = (float) WINDOW_HEIGHT / ROWS;
-
-
     //render horizontal lines
-    for(int i = 0; i<ROWS; i++) {
-        SDL_FillRect(plug->global_surface, &(SDL_Rect){0,i*cell_length,  WINDOW_WIDTH, GRID_THICKNESS}, COLOR_GREY);
+    for(int i = 0; i*CELL_WIDTH < WINDOW_HEIGHT; i++) {
+        SDL_FillRect(plug->global_surface, &(SDL_Rect){0,i*CELL_WIDTH,  WINDOW_WIDTH, GRID_THICKNESS}, COLOR_GREY);
     }
 
     //render te vertical lines
-    for(int i = 0; i*cell_length < WINDOW_WIDTH; i++) {
-        printf("Trying to render columns\n");
-        SDL_FillRect(plug->global_surface, &(SDL_Rect){i*cell_length,0,  GRID_THICKNESS,WINDOW_HEIGHT}, COLOR_GREY);
+    for(int i = 0; i*CELL_WIDTH < WINDOW_WIDTH; i++) {
+        SDL_FillRect(plug->global_surface, &(SDL_Rect){i*CELL_WIDTH,0,  GRID_THICKNESS,WINDOW_HEIGHT}, COLOR_GREY);
     }
+
+    draw_cell(plug->global_surface, 20, 20);
+    draw_cell(plug->global_surface, 22, 24);
+    draw_cell(plug->global_surface, 25, 21);
+}
+
+
+
+void draw_cell(SDL_Surface* surface, int cell_x, int cell_y) {
+    float pixel_x = cell_x * CELL_WIDTH;
+    float pixel_y = cell_y * CELL_WIDTH;
+
+    SDL_Rect cell =(SDL_Rect){ pixel_x, pixel_y, CELL_WIDTH, CELL_WIDTH };
+    SDL_FillRect(surface, &cell ,COLOR_WHITE);
 }
--- plug/plug.h
+++ plug/plug.h
@@ -4,23 +4,23 @@
 
 
 //game constants:
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 800
-#define ROWS 50
-#define COLUMNS 50
-#define GRID_THICKNESS 2
+#define WINDOW_WIDTH 902
+#define WINDOW_HEIGHT 902
+#define CELL_WIDTH (30/1.5) //NOTE: the CELL_WIDTH should be chosen carefully as there shouldn't be half cells at the boundries
+#define GRID_THICKNESS 1
 
 
 //colors:
 #define COLOR_GREY 0x808080
 #define COLOR_BLACK 0x000000
+#define COLOR_WHITE  0xffffff
 
 
 
 
-typedef struct {
-  SDL_Window *global_window;
-  SDL_Surface *global_surface;   //can be obtained from the global window itself
+  typedef struct {
+    SDL_Window *global_window;
+    SDL_Surface *global_surface;   //can be obtained from the global window itself
   float dt;
 } Plug;
 
